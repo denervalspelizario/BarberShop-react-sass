b9c4711a978ae748672c250aa96e09c4ce3ccec3
denervalspelizario <denervalspelizario@gmail.com>
2022-11-13 17:11:59 -0300

    update projeto barbershop estilizacao  section mainContainer

--- barberShop/src/componentes/MainContainer.jsx
+++ barberShop/src/componentes/MainContainer.jsx
@@ -3,27 +3,27 @@ import '../styles/componentes/MainContainer.sass'
 
 function MainContainer()  {
   return (
-    <article className='header__container'>
-        <h1>Bem-vindo a Barber Shop</h1>
-        <p>Nossa barbearia sempre oferece profissionais de qualidade e estamos prontos para lidar com suas maiores expectativas.</p>
+    <article className='main__container'>
+        <h2>Bem-vindo a Barber Shop</h2>
+        <p className='main__text--bold'>Nossa barbearia sempre oferece profissionais de qualidade e estamos prontos para lidar com suas maiores expectativas.</p>
         <p>Nossos serviços são dedicados ao seu sucesso pessoa. Aqui temos uma equipe premiada que demonstrou o talento  de mestres barbeiros em vários concursos
-          de estilo. Deixe nosso barbeiro ser seu estilista pessoal e você nunca ficará desapontado 
+          de estilo. Deixe nosso barbeiro ser seu estilista pessoal e você nunca ficará desapontado. 
         </p>
-        <p>S. Kelly</p>
+        <p className='main__text--style'>S. Kelly</p>
         <section className='container__services'>
             <div className='card__services'>
                 <img src="/img/barba.jpg" alt="profissional aparando a barba do cliente"  title='BarberShop'/>
-                <h2>Barba</h2>
+                <h3>Barba</h3>
                 <p>R$50,00</p>
             </div>
             <div className='card__services'>
                 <img src="/img/cabelo.jpg" alt="profissional cortando cabelo do cliente" title='BarberShop'/>
-                <h2>Cabelo</h2>
+                <h3>Cabelo</h3>
                 <p>R$130,00</p>
             </div>            
             <div className='card__services'>
                 <img src="/img/lavaseca.jpg" alt="profissional secando o cabelo do cliente" title='BarberShop'/>
-                <h2>Barba + Cabelo</h2>
+                <h3>Barba + Cabelo</h3>
                 <p>R$180,00</p>
             </div>         
         </section>
