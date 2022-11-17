019d20bc492a0757766e8bd2f319aa56660d7153
denervalspelizario <denervalspelizario@gmail.com>
2022-11-16 23:40:31 -0300

    update projeto barbershop aplicacao de dropdown em link da nav

--- barberShop/src/componentes/Header.jsx
+++ barberShop/src/componentes/Header.jsx
@@ -1,21 +1,51 @@
 import React from 'react'
 import '../styles/componentes/Header.sass'
 
+
+
 function Header()  {
+  
   return (
     <header>
         <div className='header__container'>
             <img src="/img/barbearia-logo.png" alt="logo da barbershop" title='BarberShop' className='image__logo'/>
             <nav className='nav__menu'>
-                <ul>
-                    <li><p><a href="#servicos">Sobre</a></p></li>
-                    <li><a href="#sobre">Serviços</a></li>
-                    <li><a href="#unidades">Unidades</a></li>
-                    <li><a href="#servicos">Sociais</a></li>
-                    <li>   
-                    </li>
+              <ul class="menu__dropdown">
+                <li class="menu__dropdown__item">
+                  <a href="" class="menu__dropdown__link">Sobre</a>
+                </li>
+                <li class="menu__dropdown__item">
+                  <a href="" class="menu__dropdown__link">Contato</a>
+                </li>
+                <li class="menu__dropdown__item">
+                  <a href="" class="menu__dropdown__link">Unidades</a>
+                </li>
+                <li class="menu__dropdown__item">
+                  <a href="" class="menu__dropdown__link">Redes Sociais</a>
+                  <ul class="submenu__dropdown">
+                  <li class="submenu__dropdown--item">
+                    <a href="" class='submenu__dropdown--link'>
+                      <i class="fa-brands fa-facebook"></i>
+                    </a> 
+                  </li>
+                  <li class="submenu__dropdown--item">
+                    <a href="" class='submenu__dropdown--link'>
+                      <i class="fa-brands fa-instagram"></i>
+                    </a> 
+                  </li>
+                  <li class="submenu__dropdown--item">
+                    <a href="" class='submenu__dropdown--link'>
+                      <i class="fa-brands fa-twitter"></i>
+                    </a> 
+                  </li>
+                  <li class="submenu__dropdown--item">
+                    <a href="" class='submenu__dropdown--link'>
+                      <i class="fa-brands fa-whatsapp"></i>
+                    </a> 
+                  </li>
                 </ul>
-                
+                </li>
+              </ul>
             </nav>
         </div>
     </header>
